c5d90f9230723dc1cc2b1afc39169e4db290d9fd
Darcy Manoel <darcman00@gmail.com>
2020-03-26 00:04:07 +1030

    Convert string definitions
    ` rather than ".

--- scriptsheet.js
+++ scriptsheet.js
@@ -1,13 +1,13 @@
 function addTab(){
-	var productTitle=prompt("What is the new product?");
+	var productTitle=prompt(`What is the new product?`);
 	if(productTitle){
 		var randomHex1=Math.floor(Math.random()*8);
 		var randomHex2=Math.floor(Math.random()*8);
 		var randomHex3=Math.floor(Math.random()*8);
-		var randomColour="#"+convertToHex(randomHex1)+convertToHex(randomHex2)+convertToHex(randomHex3);
-		var newProduct=document.createElement("div");
-		newProduct.setAttribute("class","tab");
-		newProduct.setAttribute("style","box-shadow:0px 0px 40px #000,inset 0px 0px 100px "+randomColour);
+		var randomColour=`#`+convertToHex(randomHex1)+convertToHex(randomHex2)+convertToHex(randomHex3);
+		var newProduct=document.createElement(`div`);
+		newProduct.setAttribute(`class`,`tab`);
+		newProduct.setAttribute(`style`,`box-shadow:0px 0px 40px #000,inset 0px 0px 100px;`+randomColour);
 		newProduct.innerHTML=`
 			<table class="product">
 				<tr>
@@ -24,29 +24,29 @@ function addTab(){
 					</td>
 				</tr>
 			</table>
-			`;
-		var addProduct=document.querySelector("#ghost");
+		`;
+		var addProduct=document.querySelector(`#ghost`);
 		addProduct.parentNode.insertBefore(newProduct,addProduct);
 	}
 }
 function convertToHex(hex16){
 	var hexValues=[
-		"0",
-		"1",
-		"2",
-		"3",
-		"4",
-		"5",
-		"6",
-		"7",
-		"8",
-		"9",
-		"a",
-		"b",
-		"c",
-		"d",
-		"e",
-		"f"
+		`0`,
+		`1`,
+		`2`,
+		`3`,
+		`4`,
+		`5`,
+		`6`,
+		`7`,
+		`8`,
+		`9`,
+		`a`,
+		`b`,
+		`c`,
+		`d`,
+		`e`,
+		`f`
 	];
 	return hexValues[hex16];
 }

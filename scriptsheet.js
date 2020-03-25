c46d6a122cc4245eaf8796c98c0d67466064befb
Darcy Manoel <darcman00@gmail.com>
2020-03-25 23:56:15 +1030

    Create basic interation
    You can now choose the name of the entry, it cancels the action if nothing is entered.

--- scriptsheet.js
+++ scriptsheet.js
@@ -1,14 +1,33 @@
 function addTab(){
-	var randomHex1=Math.floor(Math.random()*8);
-	var randomHex2=Math.floor(Math.random()*8);
-	var randomHex3=Math.floor(Math.random()*8);
-	var randomColour="#"+convertToHex(randomHex1)+convertToHex(randomHex2)+convertToHex(randomHex3);
-	var newTab=document.createElement("div");
-	newTab.setAttribute("class","tab");
-	newTab.setAttribute("style","box-shadow:0px 0px 40px #000,inset 0px 0px 100px "+randomColour);
-	newTab.innerHTML=`<p class="title">Placeholder</p>`;
-	var addTab=document.querySelector("#ghost");
-	addTab.parentNode.insertBefore(newTab,addTab);
+	var productTitle=prompt("What is the new product?");
+	if(productTitle){
+		var randomHex1=Math.floor(Math.random()*8);
+		var randomHex2=Math.floor(Math.random()*8);
+		var randomHex3=Math.floor(Math.random()*8);
+		var randomColour="#"+convertToHex(randomHex1)+convertToHex(randomHex2)+convertToHex(randomHex3);
+		var newProduct=document.createElement("div");
+		newProduct.setAttribute("class","tab");
+		newProduct.setAttribute("style","box-shadow:0px 0px 40px #000,inset 0px 0px 100px "+randomColour);
+		newProduct.innerHTML=`
+			<table class="product">
+				<tr>
+					<td class="productName" colspan="2">
+						`+productTitle+`
+					</td>
+				</tr>
+				<tr>
+					<td class="productFloating">
+						0
+					</td>
+					<td class="productTotal">
+						0
+					</td>
+				</tr>
+			</table>
+			`;
+		var addProduct=document.querySelector("#ghost");
+		addProduct.parentNode.insertBefore(newProduct,addProduct);
+	}
 }
 function convertToHex(hex16){
 	var hexValues=[
